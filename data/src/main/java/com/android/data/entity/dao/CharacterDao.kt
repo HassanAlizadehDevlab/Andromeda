133efe1245206bb19f7c54ce0e9dc8cd28a79035
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-09-05 20:01:38 +0430

    Implement dao for character table

---
+++ data/src/main/java/com/android/data/entity/dao/CharacterDao.kt
@@ -0,0 +1,28 @@
+package com.android.data.entity.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.android.data.entity.model.local.CharacterEntity
+import io.reactivex.Flowable
+import io.reactivex.Single
+
+/**
+ * Created by hassanalizadeh on 05,September,2020
+ */
+@Dao
+interface CharacterDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(venues: List<CharacterEntity>)
+
+    @Query("SELECT * FROM character WHERE id=:id")
+    fun selectById(id: Int): Single<CharacterEntity>
+
+    @Query("SELECT * FROM character")
+    fun selectAll(): Flowable<List<CharacterEntity>>
+
+    @Query("DELETE FROM character")
+    fun deleteAll()
+}
