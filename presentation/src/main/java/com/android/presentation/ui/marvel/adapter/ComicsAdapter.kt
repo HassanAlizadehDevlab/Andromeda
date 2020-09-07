146503803dca824a124f973a7f120bb2509df11b
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-09-07 16:27:24 +0430

    Implement character and comic adapters

---
+++ presentation/src/main/java/com/android/presentation/ui/marvel/adapter/ComicsAdapter.kt
@@ -0,0 +1,43 @@
+package com.android.presentation.ui.marvel.adapter
+
+import android.view.ViewGroup
+import com.android.domain.entity.ComicObject
+import com.android.presentation.adapter.*
+import com.android.presentation.common.extension.inflate
+
+/**
+ * Created by hassanalizadeh on 31,August,2020
+ */
+class ComicsAdapter(
+    private val listener: (BaseViewHolder<*>) -> Unit
+) : BaseRecyclerAdapter(CONFIG) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        val view = parent.inflate(viewType)
+        val holder = when (viewType) {
+            ViewTypeHolder.COMIC_VIEW -> ComicViewHolder(view)
+            ViewTypeHolder.LOAD_MORE_VIEW -> LoadMoreViewHolder(view)
+            else -> EmptyViewHolder(view)
+        }
+        listener(holder)
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        super.onBindViewHolder(holder, position)
+
+        val data = mItems[position]
+        when (holder.getType()) {
+            ViewTypeHolder.COMIC_VIEW -> (holder as ComicViewHolder).bind(data as? ComicObject)
+            ViewTypeHolder.LOAD_MORE_VIEW -> (holder as LoadMoreViewHolder).bind(Unit)
+        }
+    }
+
+    companion object {
+        val CONFIG = Config.Builder()
+            .setPreFetch(5)
+            .setScreenSize(3)
+            .build()
+    }
+
+}
