ca00c38de19fccc6ef4c738420aa9f3186f38c26
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-09-09 19:20:34 +0430

    Add fragment to activity and implement my own scroll listener

--- presentation/src/main/java/com/android/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/android/presentation/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.android.presentation.ui
 import android.os.Bundle
 import com.android.presentation.R
 import com.android.presentation.common.view.BaseActivity
+import com.android.presentation.ui.marvel.MarvelView
+import com.android.presentation.ui.marvel.MarvelViewImpl
 
 /**
  * Created by hassanalizadeh on 28,August,2020
@@ -13,7 +15,8 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // if (savedInstanceState == null)
+        if (savedInstanceState == null)
+            addFragment(R.id.fragmentContainer, MarvelViewImpl.newInstance())
             // Add fragment here
     }
 
--- presentation/src/main/java/com/android/presentation/ui/marvel/MarvelViewImpl.kt
+++ presentation/src/main/java/com/android/presentation/ui/marvel/MarvelViewImpl.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.android.domain.entity.CharacterObject
 import com.android.domain.entity.ComicObject
 import com.android.presentation.R
+import com.android.presentation.common.extension.linearLayout
+import com.android.presentation.common.utils.Utils
 import com.android.presentation.common.view.BaseFragment
+import com.android.presentation.ui.marvel.adapter.CharactersAdapter
+import com.android.presentation.ui.marvel.adapter.ComicsAdapter
+import kotlinx.android.synthetic.main.fragment_marvel.*
 import javax.inject.Inject
 
 /**
@@ -17,7 +24,18 @@ class MarvelViewImpl : BaseFragment(), MarvelView {
 
     @Inject
     lateinit var presenter: MarvelPresenter
+    private lateinit var charactersAdapter: CharactersAdapter
+    private lateinit var comicsAdapter: ComicsAdapter
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        charactersAdapter = CharactersAdapter { holder ->
+            presenter.observeClicks(holder.observe())
+        }
+        comicsAdapter = ComicsAdapter { holder -> }
+        presenter.loadMoreCharacters(charactersAdapter.getLoadMoreObservable())
+        presenter.loadMoreComics(comicsAdapter.getLoadMoreObservable())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,20 +43,54 @@ class MarvelViewImpl : BaseFragment(), MarvelView {
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_marvel, container, false)
 
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        setupRecyclerView()
+        setupAdapter()
+
+        presenter.refresh()
+    }
+
     override fun setCharacters(items: List<CharacterObject>) {
-        TODO("Not yet implemented")
+        charactersAdapter.addItems(items.toMutableList())
     }
 
     override fun setComics(items: List<ComicObject>) {
-        TODO("Not yet implemented")
+        comicsAdapter.addItems(items.toMutableList())
     }
 
     override fun loading(status: Boolean) {
-        TODO("Not yet implemented")
+
     }
 
     override fun comicsLoading(status: Boolean) {
-        TODO("Not yet implemented")
+
+    }
+
+
+    private fun setupRecyclerView() {
+        recyclerViewCharacters?.linearLayout(
+            context = activityContext,
+            orientation = RecyclerView.HORIZONTAL,
+            noTopSpacingForFirstItem = true,
+            spacing = Utils.convertDpToPx(activityContext, 4f).toInt()
+        )
+
+        recyclerViewComics?.linearLayout(
+            context = activityContext,
+            spacing = Utils.convertDpToPx(activityContext, 4f).toInt()
+        )
+    }
+
+    private fun setupAdapter() {
+        recyclerViewCharacters?.adapter = charactersAdapter
+        recyclerViewComics?.adapter = comicsAdapter
+    }
+
+
+    companion object {
+        fun newInstance(): Fragment = MarvelViewImpl()
     }
 
 }
